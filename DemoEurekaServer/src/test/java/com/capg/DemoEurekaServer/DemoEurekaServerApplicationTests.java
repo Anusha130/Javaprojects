cbce0f80e6b43f7c145179eee4448007a232dbe9
Anusha130 <102415619+Anusha130@users.noreply.github.com>
2025-01-23 19:32:27 +0530

    Add files via upload

---
+++ DemoEurekaServer/src/test/java/com/capg/DemoEurekaServer/DemoEurekaServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.capg.DemoEurekaServer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoEurekaServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
